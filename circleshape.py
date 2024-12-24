8505817c316c3dfe5911c6d9746eec8b6e2bc38a
gamepro250 <gamepro250@verizon.net>
2024-12-24 12:55:10 -0500

    Completed basic Asteroids game

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collided(self, shape):
+        distance = self.position.distance_to(shape.position)
+
+        if(distance <= (self.radius + shape.radius)):
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
@@ -9,7 +13,14 @@ def main():
     dt = 0
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
+    Shot.containers = (updateable, drawable, shots)
+
+    asteroid_field = AsteroidField()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -20,7 +31,17 @@ def main():
         
         for entity in updateable:
             entity.update(dt)
-            
+
+        for asteroid in asteroids:
+            if asteroid.collided(player):
+                print("Game over!")
+                sys.exit()
+        
+            for shot in shots:
+                if(asteroid.collided(shot)):
+                    asteroid.split()
+                    shot.kill()
+
         screen.fill("black")
         for entity in drawable:
            entity.draw(screen)
